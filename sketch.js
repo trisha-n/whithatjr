5df8a6b96bae550b1ab8534c6e0987ecb0e8cceb
trisha-n <64424670+trisha-n@users.noreply.github.com>
2020-07-02 21:54:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var ball,img,paddle;
+function preload() {
+  /* preload your images here of the ball and the paddle */
+}
+function setup() {
+  createCanvas(400, 400);
+   /* create the Ball Sprite and the Paddle Sprite */
+  /* assign the images to the sprites */
+  
+  /* give the ball an initial velocity of 9 in the X direction */
+  
+
+}
+
+function draw() {
+  background(205,153,0);
+  /* create Edge Sprites here */
+  
+  /* Allow the ball sprite to bounceOff the left, top and bottom edges only, leaving the right edge of the canvas to be open. */
+  
+
+  /* Allow the ball to bounceoff from the paddle */
+  /* Also assign a collision callback function, so that the ball can have a random y velocity, making the game interesting */
+ 
+  /* Prevent the paddle from going out of the edges */ 
+  
+  
+  if(keyDown(UP_ARROW))
+  {
+     /* what should happen when you press the UP Arrow Key */
+  }
+  
+  if(keyDown(DOWN_ARROW))
+  {
+    /* what should happen when you press the UP Arrow Key */
+  }
+  drawSprites();
+  
+}
+
+function randomVelocity()
+{
+  /* this function gets called when the ball bounces off the paddle */
+  /* assign the ball a random vertical velocity, so it bounces off in random direction */
+}
+
